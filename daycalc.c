d8a73d57c2a63f5baca49c8ed2eb166e2e2bb646
Sheron <sheronroy2003@gmail.com>
2024-08-19 00:06:30 +0530

    Improved logic , O(1) achieved !

--- daycalc.c
+++ daycalc.c
@@ -1,71 +1,58 @@
 #include<stdio.h>
-void dayselect(int day){
-	(day<0)?printf("ERROR! : "):printf("Its a ");
-	switch(day){
-		case 0:printf("Sunday");
-					 break;
-		case 1:printf("Monday");
-					 break;
-		case 2:printf("Tuesday");
-					 break;
-		case 3:printf("Wednesday");
-					 break;
-		case 4:printf("Thursday");
-					 break;
-		case 5:printf("Friday");
-					 break;
-		case 6:printf("Saturday");
-					 break;
-		default:printf("Invalid input\n");
-	}
-}
-void inyearchange(int ych,int year,int l){
-	int jan1=6,offsetof[13]={0,0,3,3,6,1,4,6,2,5,0,3,5},m[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
+int in_yr_change(int no_yr_offsets,int year,int leap, int month, int day){
+	int jan1=6;
 	if(year<2000){
-		jan1+=7;
-		ych*=(-1);
+		jan1 += 7;
+		no_yr_offsets *= (-1);
 	}
-	int month,day,leap=0;
+	if(month < 3)
+		leap = 0;
+	day--;
+	int offsetof[12]={0,3,3,6,1,4,6,2,5,0,3,5};
+	return (jan1 + no_yr_offsets + offsetof[month - 1] + leap + day) % 7;
+}
+int is_valid(int month, int day, int leap){
+	int no_days[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+	if(month < 1 || month > 12 || day < 1)
+		return 0;
+	if(month != 2){
+		if(day > no_days[month - 1])
+			return 0;
+	}
+	else if((leap) ? day > 29 : day > 28)
+		return 0;
+	return 1;
+}
+int n_yr_offsets(int inp, int leap){
+	int temp = 2000 - inp;	
+	int leap_of_2000 = 0, no_leap_yrs = temp / 4 - temp / 100 + temp / 400; 
+	if(temp < 0){
+		temp *= -1;
+		no_leap_yrs *= -1;
+		leap_of_2000 = 1;
+		if(leap)
+			no_leap_yrs-=1;
+	}
+	int total = temp;
+	return ((no_leap_yrs * 2) + (total - no_leap_yrs + leap_of_2000)) % 7; 
+}
+int main(int argc, char *argv[])
+{
+	int inp;
+	printf("Enter the input year : ");
+	scanf("%d",&inp);
+	int month,day;
 	printf("Enter the Month : ");
 	scanf("%d",&month);
 	printf("Enter the Day : ");
 	scanf("%d",&day);
-	if(month>2&&l)
-		leap=1;
-	if(month<1||month>12||day<1)
-		jan1=-1;
-	if(month!=2)
-		jan1=(day>m[month])?(-1):jan1;
-	else if((l)?day>29:day>28)
-		jan1=(-1);
-	day--;
-	if(jan1!=(-1))
-		jan1=(jan1+ych+offsetof[month]+leap+day)%7;
-	dayselect(jan1);
-}
-void yearchange(int inp){
-	int preset=2000,mod=100,out=20,initcount,inmod,total,off=0,leap;
-	inmod=inp/mod;
-	out-=inmod;
-	initcount=preset-inp;
-	if(initcount<0)
-		initcount*=-1,out*=-1,off=1;
-	total=initcount;
-	initcount/=4;		
-	for(int i=0;i!=out;i+=1){
-		if(i%4!=0)
-			initcount-=1;
+	int leap = inp % 100 == 0 ? inp % 400 == 0 : inp % 4 == 0;
+	if(is_valid(month,day,leap)){
+		int no_yr_offsets = n_yr_offsets(inp, leap);
+		day = in_yr_change(no_yr_offsets, inp, leap, month, day);
+		char days[7][10] = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+		printf("Its a %s\n",days[day]);
 	}
-	leap=inp%100==0?inp%400==0:inp%4==0;
-	if(off&&leap)
-		initcount-=1;
-	initcount=((initcount*2)+(total-initcount+off))%7;
-	inyearchange(initcount,inp,leap);
-}
-int main(int argc, char *argv[])
-{
-	int inp,leapcount;
-	printf("Enter the input year : ");
-	scanf("%d",&inp);
-	yearchange(inp);
+	else
+		printf("ERROR : Invalid input\n");	
 }
